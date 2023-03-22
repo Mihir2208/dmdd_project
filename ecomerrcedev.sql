041fd745e30dbde3ddedc145c8e34fd54d350e8f
Mihir Makwana <mihirmakwana@Mihirs-MacBook-Pro.local>
2023-03-22 16:33:25 -0400

    Created 3 tables Customer, Customer_Address, Payment_Type

--- ecomerrcedev.sql
+++ ecomerrcedev.sql
@@ -1,19 +1,33 @@
+
+
+DROP TABLE customer_address;
+DROP TABLE payment_type;
+DROP TABLE customer;
+
+--Create Customer Table
 create table customer(
-CustomerID number(10),
-FirstName varchar2(10),
-LastName varchar2(10));
+CustomerID number(38) PRIMARY KEY,
+Customer_FirstName varchar2(50),
+Customer_LastName varchar2(20),
+Customer_DOB date,
+Customer_PhoneNo varchar2(20),
+Customer_Email varchar2(100));
 
 desc customer;
 
-SELECT
-  table_name, owner
-FROM
-  all_tables
-ORDER BY
-  owner, table_name;
-  
-SELECT object_name,created FROM user_objects
-WHERE object_name LIKE  'CUSTOMER'
-AND object_type = 'TABLE'; 
+--Create Customer Address Table
+create table customer_address(
+CustomerAdd_ID number(38) PRIMARY KEY,
+CustomerID number(38) REFERENCES customer (CustomerID),
+Street varchar2(200),
+City varchar2(50),
+Zip number(38));
 
-DROP TABLE customer;
+desc customer_address;
+
+--Create Payment Type
+create table payment_type(
+Payment_type_ID number(38) PRIMARY KEY,
+Payment_type_name varchar2(50));
+
+desc payment_type;
