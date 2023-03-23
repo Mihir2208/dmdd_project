48c5ef48fdba42201bb3aebc99d0d26ca578d55d
MeghanaNandaKumar <nandakumar.me@northeastern.edu>
2023-03-23 17:48:43 -0400

    Added delivery partner and payment type table data

---
+++ 1ecommerce_dev.sql
@@ -0,0 +1,64 @@
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('101', 'cash' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('102', 'credit' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('103', 'bank' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('104', 'cash' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('105', 'cash' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('106', 'crypto' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('107', 'ewallet' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('108', 'cash' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('109', 'credit' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('110', 'bank' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('111', 'bank' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('112', 'credit' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('113', 'debit_card' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('114', 'credit' ); 
+Insert into payment_type
+(payment_type_id, payment_type_name) values ('115', 'debit_card' );
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 121, 'Jacob', '2067086899', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 122, 'Jake', '2067086900', 'jac1234@gmail.com'); 
+ 
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 123, 'Ell', '2067086901', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 124, 'Jones', '2067086902', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 125, 'Paul', '2067086903', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 126, 'Richard', '2067086904', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 127, 'Joan', '2067086905', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 128, 'Kerry', '2067086906', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 129, 'Miranda', '2067086907', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 130, 'Patricia', '2067086908', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 131, 'Ben', '2067086909', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 132, 'Barbara', '2067086910', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 133, 'Meg', '2067086911', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 134, 'Sloan', '2067086912', 'jac1234@gmail.com'); 
+
+Insert into delivery_partner(delivery_partner_id, delivery_partner_name, delivery_partner_phoneno, deliver_partner_email) values ( 135, 'Jim', '2067086913', 'jac1234@gmail.com'); 
+
+select * from payment_type;
+select * from delivery_partner;
+
