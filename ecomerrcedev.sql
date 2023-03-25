6df87382c51b8ac1f89fa7fa8c262b13b6417a6a
Mihir Makwana <mihirmakwana@Mihirs-MacBook-Pro.local>
2023-03-25 13:50:08 -0400

    Created Tables using the scripts

--- ecomerrcedev.sql
+++ ecomerrcedev.sql
@@ -1,114 +1,134 @@
-DROP TABLE customer_address;
-DROP TABLE order_delivery_status;
-DROP TABLE order_details;
-DROP TABLE customer_order;
-DROP TABLE payment_type;
-DROP TABLE customer;
-alter table distributor drop COLUMN Product_manufacturer_ID;
-alter table product_manufacturer drop COLUMN Distributor_ID;
-DROP TABLE product_manufacturer;
-DROP TABLE distributor;
-DROP TABLE product;
-DROP TABLE product_type;
-DROP TABLE delivery_partner;
+--CLEANUP SCRIPT
+set serveroutput on
+declare
+    v_table_exists varchar(1) := 'Y';
+    v_sql varchar(2000);
+begin
+   dbms_output.put_line('Start schema cleanup');
+             
+      execute immediate 'alter table DISTRIBUTOR drop column Product_manufacturer_ID';
+      execute immediate 'alter table PRODUCT_MANUFACTURER drop column Distributor_ID';
+      
+      
+   for i in (select 'CUSTOMER_ADDRESS' table_name from dual union all
+             select 'ORDER_DELIVERY_STATUS' table_name from dual union all
+             select 'ORDER_DETAILS' table_name from dual union all
+             select 'CUSTOMER_ORDER' table_name from dual union all
+             select 'PAYMENT_TYPE' table_name from dual union all
+             select 'CUSTOMER' table_name from dual union all
+             select 'PRODUCT_MANUFACTURER' table_name from dual union all
+             select 'DISTRIBUTOR' table_name from dual union all
+             select 'PRODUCT' table_name from dual union all
+             select 'PRODUCT_TYPE' table_name from dual union all
+             select 'DELIVERY_PARTNER' table_name from dual
+   )
+   loop
+   dbms_output.put_line('....Drop table '||i.table_name);
+   begin
+       select 'Y' into v_table_exists
+       from USER_TABLES
+       where TABLE_NAME=i.table_name;
+
+       v_sql := 'drop table '||i.table_name;
+       execute immediate v_sql;
+       dbms_output.put_line('........Table '||i.table_name||' dropped successfully');
+       
+   exception
+       when no_data_found then
+           dbms_output.put_line('........Table already dropped');
+   end;
+   end loop;
+   dbms_output.put_line('Schema cleanup successfully completed');
+exception
+   when others then
+      dbms_output.put_line('Failed to execute code:'||sqlerrm);
+end;
+/
+
+--CREATE TABLES AS PER DATA MODEL
 
 
---Create Customer Table
 create table customer(
 CustomerID number(38) PRIMARY KEY,
 Customer_FirstName varchar2(50),
 Customer_LastName varchar2(20),
 Customer_DOB date,
 Customer_PhoneNo varchar2(20),
-Customer_Email varchar2(100));
+Customer_Email varchar2(100)
+)
+/
 
---desc customer;
-
---Create Customer Address Table
 create table customer_address(
 CustomerAdd_ID number(38) PRIMARY KEY,
 CustomerID number(38) REFERENCES customer (CustomerID),
 Street varchar2(200),
+State varchar2(200),
 City varchar2(50),
-Zip number(38));
+Zip number(38)
+)
 
---desc customer_address;
+/
 
---Create Payment Type
 create table payment_type(
 Payment_type_ID number(38) PRIMARY KEY,
-Payment_type_name varchar2(50));
+Payment_type_name varchar2(50))
+/
 
---desc payment_type;
-
---Create ProductType Table
 create table product_type(
 Product_type_ID number(38) PRIMARY KEY,
 Product_type_name varchar(50),
-Product_Description varchar(500));
+Product_Description varchar(500))
+/
 
---desc product_type;
-
---Create Product Table
 create table product(
 Product_ID number(38) PRIMARY KEY,
 Product_type_ID number(38) REFERENCES product_type (Product_type_ID),
 Product_name varchar2(200),
 Product_cost number(38),
 Product_active varchar2(20),
-Product_quantity number(38));
+Product_quantity number(38))
+/
 
---desc product;
-
-
---Create Distributor Table
 create table distributor(
 Distributor_ID number (38) PRIMARY KEY,
 Product_ID number(38),
 Product_manufacturer_ID number(38),
 Product_Quantity number(38),
-CONSTRAINT FK_Product_ID FOREIGN KEY (Product_ID) REFERENCES product(Product_ID));
+CONSTRAINT FK_Product_ID FOREIGN KEY (Product_ID) REFERENCES product(Product_ID))
+/
 
---desc distributor;
-
-
---Create Product Manufacturer Table
 create table product_manufacturer(
 Product_manufacturer_ID number(38) PRIMARY KEY,
 Distributor_ID number(38),
-CONSTRAINT FK_Distributor_ID FOREIGN KEY (Distributor_ID) REFERENCES distributor(Distributor_ID));
+CONSTRAINT FK_Distributor_ID FOREIGN KEY (Distributor_ID) REFERENCES distributor(Distributor_ID))
+/
 
 alter table distributor add CONSTRAINT FK_Product_manufacturer_ID FOREIGN KEY (Product_manufacturer_ID) REFERENCES product_manufacturer(Product_manufacturer_ID);
 
---Create Customer Order Table 
 create table customer_order(
 Order_ID number(38) PRIMARY KEY,
 Order_date date,
 Payment_type_ID number(38) REFERENCES payment_type (Payment_type_ID),
-CustomerID number(38) REFERENCES customer (CustomerID));
+CustomerID number(38) REFERENCES customer (CustomerID))
+/
 
---desc customer_order;
-
---Create Order Details Table
 create table order_details(
 Order_details_ID number(38) PRIMARY KEY,
 Product_ID number(38) REFERENCES product (Product_ID),
-Order_ID number(38) REFERENCES customer_order (Order_ID));
+Order_ID number(38) REFERENCES customer_order (Order_ID))
+/
 
---desc order_details;
-
---Create Order Delivery Partner
 create table delivery_partner(
 Delivery_partner_ID number(38) PRIMARY KEY,
 Delivery_partner_Name varchar2(50),
 Delivery_partner_PhoneNo varchar2(20),
-Deliver_partner_Email varchar2(100));
-
---desc delivery_partner;
+Deliver_partner_Email varchar2(100))
+/
 
 create table order_delivery_status(
 Order_delivery_status_ID number(38) PRIMARY KEY,
 Order_delivery_status_Name varchar2(50),
 Order_ID number(38) REFERENCES customer_order (Order_ID),
 Delivery_partner_ID number(38) REFERENCES delivery_partner (Delivery_partner_ID)
-);
+)
+/
