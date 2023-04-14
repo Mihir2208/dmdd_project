585c09a7dfb625f2ea8f83751d23d756e6c8321d
Pratik Poojari <poojari.pr@northeastern.edu>
2023-04-14 15:28:55 -0400

    Procedure for CustomerReport of orders placed between a specific date

--- FinalEcommerceDB.sql
+++ FinalEcommerceDB.sql
@@ -125,3 +125,49 @@ END;
 
 exec distributor_inventory_report;
 
+
+--8 Procedure for CustomerReport of orders placed between a specific date
+set serveroutput on;
+CREATE OR REPLACE PROCEDURE customer_order_report(
+    p_customer_id IN customer.CustomerID%TYPE,
+    p_start_date IN customer_order.Order_date%TYPE,
+    p_end_date IN customer_order.Order_date%TYPE,
+    p_product_type_id IN product_type.Product_type_ID%TYPE DEFAULT NULL
+) AS
+    CURSOR customer_order_cursor IS
+        SELECT co.Order_ID, c.Customer_FirstName, c.Customer_LastName, c.Customer_PhoneNo, c.Customer_Email,
+               cadd.Street, cadd.State, cadd.City, cadd.Zip, co.Order_date, pt.Payment_type_name,
+               p.Product_name, p.Product_cost, od.Product_ID, od.Order_details_ID, ods.Order_delivery_status_Name,
+               dp.Delivery_partner_Name, dp.Delivery_partner_PhoneNo
+        FROM customer_order co
+        INNER JOIN customer c ON co.CustomerID = c.CustomerID
+        INNER JOIN customer_address cadd ON co.CustomerID = cadd.CustomerID
+        INNER JOIN payment_type pt ON co.Payment_type_ID = pt.Payment_type_ID
+        INNER JOIN order_details od ON co.Order_ID = od.Order_ID
+        INNER JOIN product p ON od.Product_ID = p.Product_ID
+        INNER JOIN product_type pt2 ON p.Product_type_ID = pt2.Product_type_ID
+        INNER JOIN order_delivery_status ods ON co.Order_ID = ods.Order_ID
+        INNER JOIN delivery_partner dp ON ods.delivery_partner_id = dp.delivery_partner_id
+        WHERE c.CustomerID = p_customer_id
+          AND co.Order_date >= p_start_date
+          AND co.Order_date <= p_end_date
+          AND (p_product_type_id IS NULL OR p_product_type_id = pt2.Product_type_ID)
+        ORDER BY co.Order_date DESC;
+
+    v_total_cost NUMBER(38,2) := 0;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Order ID | Customer Name | Customer Phone | Customer Email | Street | State | City | Zip | Order Date | Payment Type | Product Name | Product Cost | Product ID | Order Details ID | Delivery Status | Delivery Partner Name | Delivery Partner Phone');
+
+    FOR cust_order IN customer_order_cursor LOOP
+        DBMS_OUTPUT.PUT_LINE(cust_order.Order_ID || ' | ' || cust_order.Customer_FirstName || ' ' || cust_order.Customer_LastName || ' | ' || cust_order.Customer_PhoneNo || ' | ' || cust_order.Customer_Email || ' | ' || cust_order.Street || ' | ' || cust_order.State || ' | ' || cust_order.City || ' | ' || cust_order.Zip || ' | ' || cust_order.Order_date || ' | ' || cust_order.Payment_type_name || ' | ' || cust_order.Product_name || ' | ' || cust_order.Product_cost || ' | ' || cust_order.Product_ID || ' | ' || cust_order.Order_details_ID || ' | ' || cust_order.Order_delivery_status_Name || ' | ' || cust_order.Delivery_partner_Name || ' | ' || cust_order.Delivery_partner_PhoneNo);
+
+        v_total_cost := v_total_cost + cust_order.Product_cost;
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('Total cost for orders: ' || v_total_cost);
+END;
+
+exec customer_order_report(1, '01-JAN-2022','01-JAN-2023');
+
+
+
